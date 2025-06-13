36fb757ec6c9dda25555a299a9870eac39a6609e
King-Lota <lottanna47@gmail.com>
2025-06-13 11:22:00 +0100

    users can change password

--- components/settings/components/secuirty-settings.tsx
+++ components/settings/components/secuirty-settings.tsx
@@ -3,7 +3,6 @@
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
